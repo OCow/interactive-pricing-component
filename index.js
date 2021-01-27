0c271fe5fe2f4108cd857a915a9add1f23bda7d6
Oliver Kuhn <oliver.kuhn@hella-gutmann.com>
2021-01-27 07:51:31 +0100

    add toggler

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // alert("Haha");
+
+window.onload = () => {
+  //   console.log("window.onload()");
+  let toggler = document.getElementById("discount-toggler");
+
+  if (toggler) {
+    toggler.addEventListener("change", handleTogglerChange);
+    //  console.log("Event listener added for toggler");
+  }
+};
+
+function handleTogglerChange(event) {
+  if (event.target.checked) {
+    console.log("toggle on...");
+  } else {
+    console.log("toggle off...");
+  }
+}
