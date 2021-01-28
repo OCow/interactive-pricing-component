623101e53bbfdf38ce663a9edc5296132b64b064
Oliver Kuhn <oliver.kuhn@hella-gutmann.com>
2021-01-28 08:17:04 +0100

    adapt toggler, calculate price with / without discount

--- index.js
+++ index.js
@@ -8,12 +8,34 @@ window.onload = () => {
     toggler.addEventListener("change", handleTogglerChange);
     //  console.log("Event listener added for toggler");
   }
+
+  //set base price for initial loading of site
+  document.getElementById("price-number").innerText = 16;
 };
 
+/**
+ * TODO implement...
+ * Depending on slider position, the base price differs.
+ * - 10K pageviews / $8 per month
+ * - 50K pageviews / $12 per month
+ * - 100K pageviews / $16 per month
+ * - 500k pageviews / $24 per month
+ * - 1M pageviews / $36 per month
+ */
+function getBasePriceDependingOnPageView() {
+  return 16;
+}
+
 function handleTogglerChange(event) {
+  let basePrice = getBasePriceDependingOnPageView();
+
   if (event.target.checked) {
-    console.log("toggle on...");
+    //basePrice = basePrice - basePrice * 0.25;
+    basePrice = (basePrice - basePrice * 0.33).toFixed(2);
+    //console.log(`Yearly billing - ${basePrice} $`);
   } else {
-    console.log("toggle off...");
+    //console.log(`Montly billing - ${basePrice} $`);
   }
+
+  document.getElementById("price-number").innerText = basePrice;
 }
