8e6042f0d0d24fad96a7c747c557a4b75f1c1f3a
oliver.kuhn2@gmail.com <oliver.kuhn2@gmail.com>
2021-01-29 16:31:01 +0100

    add javascript for slider, adapt page view and price

--- index.js
+++ index.js
@@ -1,41 +1,128 @@
-// alert("Haha");
-
+/**
+ * Called when browser has loaded the page. All the elements
+ * should be available by now, therefore we are adding the event
+ * handlers here.
+ */
 window.onload = () => {
   //   console.log("window.onload()");
   let toggler = document.getElementById("discount-toggler");
-
   if (toggler) {
     toggler.addEventListener("change", handleTogglerChange);
-    //  console.log("Event listener added for toggler");
   }
 
-  //set base price for initial loading of site
-  document.getElementById("price-number").innerText = 16;
+  let slider = document.getElementById("pageview-slider");
+  if (slider) {
+    //listening to mousemove enables us the change the fill state
+    //of the runnable slider area WHILE the mouse button is still
+    //down. The change event only occurs when the mouse button is
+    //released.
+    slider.addEventListener("mousemove", handleSliderEvent);
+
+    //Also listen to change event of slider. This is necessary in case
+    //the user just clicks on the runnable area and now mouse move
+    //occurs at all.
+    slider.addEventListener("change", handleSliderEvent);
+
+    //Fill the slider with the initial value. Setting this in .scss file
+    //did not seem to work. In case the .scss file also contains a background
+    //property, the filling does not work correct.
+    let value = ((slider.value - slider.min) / (slider.max - slider.min)) * 100;
+    slider.style.background = `linear-gradient(90deg, hsl(174, 77%, 80%) ${value}%, hsl(224, 65%, 95%) ${value}%)`;
+    slider.style.borderRadius = "9px";
+  }
+
+  setPriceAndPageView();
 };
 
 /**
- * TODO implement...
- * Depending on slider position, the base price differs.
- * - 10K pageviews / $8 per month
- * - 50K pageviews / $12 per month
- * - 100K pageviews / $16 per month
- * - 500k pageviews / $24 per month
- * - 1M pageviews / $36 per month
+ * Set current price depending on slider position and discount toggler
+ * state.
  */
-function getBasePriceDependingOnPageView() {
-  return 16;
-}
+function setPriceAndPageView() {
+  let pageViewValue = document.getElementById("pageview-slider").value;
+  let discount = document.getElementById("discount-toggler").checked
+    ? true
+    : false;
 
-function handleTogglerChange(event) {
-  let basePrice = getBasePriceDependingOnPageView();
+  console.log(
+    "Value: " + pageViewValue + ", discount: " + (discount ? "yes" : "no")
+  );
 
-  if (event.target.checked) {
-    //basePrice = basePrice - basePrice * 0.25;
-    basePrice = (basePrice - basePrice * 0.33).toFixed(2);
-    //console.log(`Yearly billing - ${basePrice} $`);
-  } else {
-    //console.log(`Montly billing - ${basePrice} $`);
+  let pageViewString = "";
+  let pageViewPrice = -1;
+  switch (Number(pageViewValue)) {
+    case 1:
+      {
+        pageViewString = "10 k";
+        pageViewPrice = 8;
+      }
+      break;
+
+    case 2:
+      {
+        pageViewString = "50 k";
+        pageViewPrice = 12;
+      }
+      break;
+
+    case 3:
+      {
+        pageViewString = "100 k";
+        pageViewPrice = 16;
+      }
+      break;
+
+    case 4:
+      {
+        pageViewString = "500 k";
+        pageViewPrice = 24;
+      }
+      break;
+
+    case 5:
+      {
+        pageViewString = "1 M";
+        pageViewPrice = 36;
+      }
+      break;
   }
 
-  document.getElementById("price-number").innerText = basePrice;
+  if (discount) {
+    pageViewPrice = (pageViewPrice - pageViewPrice * 0.25).toFixed(0);
+  }
+
+  document.getElementById("pageviews_amount").innerText = pageViewString;
+  document.getElementById("price-number").innerText = pageViewPrice;
+}
+
+//TODO get rid of this global variable using a closure
+let oldValue = -1;
+/**
+ * Handle slider event. Can be either "change" or "mousemove". The event
+ * handler is the same for both.
+ * @param {*} event
+ */
+function handleSliderEvent(event) {
+  //access value, min and max of slider. These value are specified in html
+  let value = ((this.value - this.min) / (this.max - this.min)) * 100;
+  // console.log("haha - " + value);
+
+  //TODO use closure for comparing this...
+  if (oldValue === -1 || oldValue !== value) {
+    setPriceAndPageView();
+  }
+
+  this.style.background = `linear-gradient(90deg, hsl(174, 77%, 80%) ${value}%, hsl(224, 65%, 95%) ${value}%)`;
+  this.style.borderRadius = "9px";
+
+  //store the current value for the next call
+  oldValue = value;
+}
+
+/**
+ * Event handler for toggler. Adapts the price field accordingly.
+ * @param {*} event
+ */
+function handleTogglerChange(event) {
+  setPriceAndPageView();
 }
